b2afe73e480c7ed2006897030deb7e397656e297
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-05 15:57:16 -0300

    Add dummy verificator

--- x/lambchain/keeper/msg_server_verify.go
+++ x/lambchain/keeper/msg_server_verify.go
@@ -2,17 +2,26 @@ package keeper
 
 import (
 	"context"
-
-	"lambchain/x/lambchain/types"
+	"fmt"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"lambchain/x/lambchain/types"
 )
 
 func (k msgServer) Verify(goCtx context.Context, msg *types.MsgVerify) (*types.MsgVerifyResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
 	_ = ctx
 
-	return &types.MsgVerifyResponse{}, nil
+	if !verify(msg.Proof) {
+		fmt.Println("Invalid proof: ", msg.Proof)
+		return nil, types.ErrSample
+	} else {
+		fmt.Println("Valid proof: ", msg.Proof)
+		return &types.MsgVerifyResponse{}, nil
+	}
+}
+
+func verify(proof string) bool {
+	return !strings.Contains(proof, "invalid")
 }
